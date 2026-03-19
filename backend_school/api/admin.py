e6fda69a87ee4f14b352b3d58274625bb915591e
Artur Z <arturziianbaev@gmail.com>
2026-03-18 20:16:45 -0700

    Add Teacher model and update User admin interface;

--- backend_school/api/admin.py
+++ backend_school/api/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Teacher
 # Register your models here.
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_student', 'is_teacher', 'is_parent', )
+    list_filter = ('is_student', 'is_teacher', 'is_parent')
+    exclude = ('password',)  # Exclude password from admin form for security reasons
+    
+admin.site.register(User, UserAdmin)
+admin.site.register(Teacher)
--- backend_school/api/models.py
+++ backend_school/api/models.py
@@ -11,3 +11,11 @@ class User(AbstractUser):
 
 	def __str__(self) -> str:
 		return self.get_full_name() or self.username
+
+class Teacher(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='teacher_profile')
+	teaching_experience = models.PositiveIntegerField(default=0)  # in years
+	description = models.TextField()
+
+	def __str__(self) -> str:
+		return f"Teacher: {self.user.get_full_name() or self.user.username}"
