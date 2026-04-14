1e19f21c4f40f3695089e462ca1fc666e01b6439
Artur Z <arturziianbaev@gmail.com>
2026-04-14 16:00:46 -0700

    Refactor signals to use AUTH_USER_MODEL and clean up unused code; add SITE_ORIGIN setting

--- backend_school/api/signals.py
+++ backend_school/api/signals.py
@@ -1,9 +1,9 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import Teacher, TrialLessonRequest
+from django.conf import settings
+from .models import TrialLessonRequest
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_user_profile(sender, instance, created, **kwargs):
     """
     Signal to create a user profile when a new user is created.
@@ -13,7 +13,7 @@ def create_user_profile(sender, instance, created, **kwargs):
         pass
 
 
-@receiver(post_delete, sender=User)
+@receiver(post_delete, sender=settings.AUTH_USER_MODEL)
 def delete_user_profile(sender, instance, **kwargs):
     """
     Signal to delete associated data when a user is deleted.
@@ -25,11 +25,12 @@ def trial_lesson_request_post_save(sender, instance, created, **kwargs):
     """
     Signal to perform actions after a TrialLessonRequest is saved.
     """
-    print(instance.status)
-    if created:
-        # Add your logic here for when a new trial lesson request is created
-        print(f'New TrialLessonRequest created with ID: {instance.id}')
-    else:
-        # Add your logic here for when a trial lesson request is updated
-        print(f'TrialLessonRequest with ID: {instance.id} has been updated')
-        print(f'Current status: {instance.status}')
+    pass
+    # print(instance.status)
+    # if created:
+    #     # Add your logic here for when a new trial lesson request is created
+    #     print(f'New TrialLessonRequest created with ID: {instance.id}')
+    # else:
+    #     # Add your logic here for when a trial lesson request is updated
+    #     print(f'TrialLessonRequest with ID: {instance.id} has been updated')
+    #     print(f'Current status: {instance.status}')
